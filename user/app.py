8db06c258eded1f27ab76040e1c67a87e29b0450
yasaman76-g <ya30gol7676@gmail.com>
2022-11-26 23:08:23 +0330

    Modify user service

--- user/app.py
+++ user/app.py
@@ -8,7 +8,7 @@ from routes import user_blueprint
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vZK7siKrV8BL46KsyAIPoQ'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/user.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/yasaman/python_projects/microservice-in-flask/user/database/user.db'
 models.init_app(app)
 app.register_blueprint(user_blueprint)
 login_manager = LoginManager(app)
